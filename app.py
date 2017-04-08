2184ad816de75aa64103c9647dca07bb3fcd1f9b
Avinash Jain <avinashkalpitjain@gmail.com>
2017-04-08 15:21:34 -0700

    Updated twilio

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from flask import Flask, request
 import twilio.twiml
-from twilio.rest import TwilioRestClient
+from twilio.rest import Client
+
 
 
 app = Flask(__name__)
 # Try adding your own number to this list!
 account_sid = "ACe7c0012d06fc3b85303d8387dffdf672"
 auth_token = "a3d0ce1a83f3d9ac352c26a95c46f96e"
-client = TwilioRestClient(account_sid, auth_token)
+client = Client(account_sid, auth_token)
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
