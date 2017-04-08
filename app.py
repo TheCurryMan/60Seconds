83a4b33afb47dfda419baaf6bd9ab1c766a05bc4
Avinash Jain <avinashkalpitjain@gmail.com>
2017-04-08 15:41:53 -0700

    Updates

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, redirect
-import twilio.twiml
+from twilio import twiml
 from twilio.rest import Client
 
 
@@ -11,25 +11,14 @@ auth_token = "a3d0ce1a83f3d9ac352c26a95c46f96e"
 client = Client(account_sid, auth_token)
 
 @app.route("/", methods=['GET', 'POST'])
-def index():
+def sms_reply():
+    """Respond to incoming calls with a simple text message."""
+    # Start our TwiML response
+    resp = twiml.Response()
 
-    print("NIKHIL U BOOSTED APE")
+    # Add a message
+    resp.message("The Robots are coming! Head for the hills!")
 
-    #Getting actual message from user
-    body = request.values.get('Body', None)
-
-    #Getting any image the user might have sent
-    img_url = request.values.get('MediaUrl0', None)
-
-    #Getting the from number from the user
-    from_number = request.values.get('From', None)
-
-    #Gets the message that's returned back to the user
-    message = "Hello"
-
-    #Sends the message back
-    resp = twilio.twiml.Response()
-    resp.message(message)
     return str(resp)
 
 if __name__ == "__main__":
