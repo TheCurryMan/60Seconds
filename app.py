956ae81fba6315d9df83caec5bb02520f2e52d8b
Nikhil Ajjarapu <nikhil.ajjarapu@gmail.com>
2017-04-08 20:46:16 -0700

    sent analysis in main v1

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, redirect, send_from_directory
 from twilio.rest import Client
 from firebase import firebase
 import datetime
+from sentanalysis import runSentimentAnalysis 
 
 
 app = Flask(__name__, static_folder='static')
@@ -26,24 +27,19 @@ def callback():
         # Initialize Firebase Application and get user data
     fb = firebase.FirebaseApplication("https://seconds-8d329.firebaseio.com/", None)
     data = fb.get('/users', None)
-
-    num = str(request.values.get("num"))
-
-
+    num = "+" + str(request.values.get("num"))
     print(num)
 
     rec2 = client.recordings.list()[0]
     finalTwilioURL = "api.twilio.com" + rec2.uri[:-4] + "mp3"
     print(finalTwilioURL)
     date = datetime.datetime.now().strftime ("%m-%d-%Y")
+    t = str(runSentimentAnalysis(finalTwilioURL))
     if num in data:
         data[num][date] = finalTwilioURL
+        data[num]["sent"] = t
     else:
-        data[num] = {date:finalTwilioURL}
-
-
-
-
+        data[num] = {date:finalTwilioURL, "sent":t}
 
     result = fb.put('', '/users', data)
 
@@ -54,7 +50,6 @@ def callback():
 @app.route("/call.xml", methods=['GET', 'POST'])
 def static_from_root():
     return send_from_directory(app.static_folder, request.path[1:])
-
 
 if __name__ == "__main__":
     app.run(debug=True)
