14117af9af04e948ac78747cd4fd2edcd9581628
Nikhil Ajjarapu <nikhil.ajjarapu@gmail.com>
2017-04-08 21:36:25 -0700

    major uphaul

--- sentanalysis.py
+++ sentanalysis.py
@@ -1,30 +1,15 @@
-import urllib3
-import speech_recognition as sr
 from textblob import TextBlob
-from pydub import AudioSegment
-import os
+from twilio.rest import Client
 
-def runSentimentAnalysis(audiourl):
-	http = urllib3.PoolManager()
-	r = http.request('GET', audiourl, preload_content=False)
 
-	with open("tmp.mp3", 'wb') as out:
-		while True:
-			data = r.read(100)
-			if not data:
-				break
-			out.write(data)
-	r.release_conn()
+def runSentimentAnalysis(sid):
+	account_sid = "ACa9eca256e7d2b82539a0c6086dc244d7"
+	auth_token = "213a8dd83633246a86c5b36361665220"
+	client = Client(account_sid, auth_token)
 
-	aud = AudioSegment.from_mp3("tmp.mp3")
-	aud.export("tmp.wav",format="wav")
-	r = sr.Recognizer()
-	audio = None
-	with sr.AudioFile("tmp.wav") as source: 
-		audio = r.listen(source)
-	txt = r.recognize_google(audio)
-	os.remove("tmp.wav")
-	os.remove("tmp.mp3")
-	return TextBlob(txt).sentiment.polarity
+	transcription_sid = sid
+	transcription = client.transcriptions(transcription_sid).fetch()
+	
+	return TextBlob(transcription).sentiment.polarity
 
-#print(runSentimentAnalysis("api.twilio.com/2010-04-01/Accounts/ACe7c0012d06fc3b85303d8387dffdf672/Recordings/REf18f17cc7638ce9aae10d95dee97bb8c.mp3"))
+#print(runSentimentAnalysis()
