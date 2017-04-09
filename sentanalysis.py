c6ae22f115ca973925f1190a588bc9f22533e6f7
Nikhil Ajjarapu <nikhil.ajjarapu@gmail.com>
2017-04-08 20:32:57 -0700

    sentiment analysis v1

--- sentanalysis.py
+++ sentanalysis.py
@@ -1,5 +1,15 @@
+import urllib.request
+import speech_recognition as sr
 from textblob import TextBlob
+from pydub import AudioSegment
 
-def runSentimentAnalysis(text):
-	blob = TextBlob(text)	
-	return blob.sentiment.polarity
+def runSentimentAnalysis(audiourl):
+	urllib.request.urlretrieve(audiourl, "tmp.mp3")
+	aud = AudioSegment.from_mp3("tmp.mp3")
+	aud.export("tmp.wav",format="wav")
+
+	r = sr.Recognizer()
+	audio = None
+	with sr.AudioFile("tmp.wav") as source: 
+		audio = r.listen(source)
+	return TextBlob(r.recognize_google(audio)).sentiment.polarity
